aa646fd601ee9df8705ba4f76ae6dcf682c50257
Sr-Van <vanderson.dev1@gmail.com>
2023-08-08 20:51:31 -0300

    responsive adjustments

--- index.js
+++ index.js
@@ -20,27 +20,88 @@ const cont1 = document.querySelector(".cont1")
 const cont2 = document.querySelector(".cont2")
 const cont3 = document.querySelector(".cont3")
 const smartWCont = document.querySelector(".cont2")
-const smartWtext = document.querySelector(".txt-img")
+const smartWText = document.querySelector(".txt-img")
 const airpoCont = document.querySelector(".cont3")
-const airpotext = document.querySelector(".txt-airpod")
+const airpoText = document.querySelector(".txt-airpod")
 
 //declaring third section const
 const thirdSecImg = document.querySelector(".cont-sec-3")
 const thirdSecHeader = document.querySelector(".header-sec-3")
 const thirdSecFooter = document.querySelector(".footer-sec-3")
 
+
+
+//functions to check and uncheck the buttons from my side navigator
+
+const checkBtn = btn =>{
+    btn.innerHTML = `
+        <span class="material-symbols-sharp">
+        radio_button_checked
+        </span>
+    `
+}
+
+const uncheckBtn = (btn1, btn2) =>{
+    const changer = `
+            <span class="material-symbols-sharp">
+            radio_button_unchecked
+            </span>
+        ` 
+    btn1.innerHTML = changer
+    btn2.innerHTML = changer
+}
+
+//function to hover the box with the card i want to show the text with the blur animation
+const hoverTextCont =  box => {
+    box.style.opacity = "1"
+    box.classList.add("blur-anim")
+
+    setTimeout(() => {
+        box.style.opacity = "0"
+        box.classList.remove("blur-anim")
+    }, 10000);
+}
+
+//functions to anim and be called the animation for each click, with timeout 3,5sec preventing spam
+const sideTextAnim = (box, timing) => {
+    box.style.animation = `sidetext ${timing}s ease-out`
+    setTimeout(() => {
+        box.style.animation = "none"
+    }, 3500);
+}
+
+const upToDownTextAnim = box =>{
+    box.style.animation = "upToDownAnim 1s ease-out"
+    setTimeout(() => {
+        box.style.animation = "none"
+    }, 4500);
+}
+
+const buyAnim = (icon, button, text) => {
+    button.style.animation = "bgColor 2.3s ease-in"
+    text.textContent = "Redirecting..."
+    setTimeout(() => {
+        text.textContent = "Thank you!"
+        button.classList.add("bg-buy-anim")
+        icon.innerHTML = "done"
+        setTimeout(() => {
+            text.textContent = "Buy your Dreams!"
+            button.classList.remove("bg-buy-anim")
+            icon.style.transform = "translateX(0px)"
+            icon.innerHTML = "shopping_bag"
+        }, 2000)
+        icon.style.transform = "translateX(150px)"
+    }, 2000);
+}
+
 //hover effect on the second section two cards
-smartWCont.addEventListener("mouseover", ()=>{
-    hoverTextCont(smartWtext)
+smartWCont.addEventListener("mouseover",  ( ) => {
+    hoverTextCont(smartWText)
 })
 
-airpoCont.addEventListener("mouseover", ()=>{
-    hoverTextCont(airpotext)
-})
+airpoCont.addEventListener("mouseover", () =>  hoverTextCont(airpoText))
 
-btnHeader.addEventListener("click", ()=>{
-    buyAnim(buyIcon, btnHeader, buyText)
-})
+btnHeader.addEventListener("click", () => buyAnim(buyIcon, btnHeader, buyText))
 
 //buttons with event listener to when clicked, call some functions with some props
 
@@ -70,75 +131,19 @@ btnAside3.addEventListener("click", ()=>{
 btnDown1.addEventListener("click", ()=>{
     checkBtn(btnAside2)
     uncheckBtn(btnAside1, btnAside3)
+    sideTextAnim(cont1, 1)
+    sideTextAnim(cont2, 2)
+    sideTextAnim(cont3, 2)
 })
 
 btnDown2.addEventListener("click", ()=>{
     checkBtn(btnAside3)
     uncheckBtn(btnAside1, btnAside2)
+    sideTextAnim(thirdSecImg, 3)
+    upToDownTextAnim(thirdSecHeader)
+    sideTextAnim(thirdSecFooter, 1.5)
 })
 
-//functions to check and uncheck the buttons from my side navigator
-
-function checkBtn(btn){
-    btn.innerHTML = `
-        <span class="material-symbols-sharp">
-        radio_button_checked
-        </span>
-    `
-}
-
-function uncheckBtn(btn1, btn2){
-    let changer = `
-            <span class="material-symbols-sharp">
-            radio_button_unchecked
-            </span>
-        ` 
-    btn1.innerHTML = changer
-    btn2.innerHTML = changer
-}
-
-//function to hover the box with the card i want to show the text with the blur animation
-function hoverTextCont(box){
-    box.style.opacity = "1"
-    box.classList.add("blur-anim")
-
-    setTimeout(() => {
-        box.style.opacity = "0"
-        box.classList.remove("blur-anim")
-    }, 10000);
-}
-
-//functions to anim and be called the animation for each click, with timeout 3,5sec preventing spam
-function sideTextAnim(box, timing){
-    box.style.animation = `sidetext ${timing}s ease-out`
-    setTimeout(() => {
-        box.style.animation = "none"
-    }, 3500);
-}
-
-function upToDownTextAnim(box){
-    box.style.animation = "upToDownAnim 1s ease-out"
-    setTimeout(() => {
-        box.style.animation = "none"
-    }, 4500);
-}
-
-function buyAnim(icon, button, text){
-    button.style.animation = "bgColor 2.3s ease-in"
-    text.textContent = "Redirecting..."
-    setTimeout(() => {
-        text.textContent = "Thank you!"
-        button.classList.add("bg-buy-anim")
-        icon.innerHTML = "done"
-        setTimeout(() => {
-            text.textContent = "Buy your Dreams!"
-            button.classList.remove("bg-buy-anim")
-            icon.style.transform = "translateX(0px)"
-            icon.innerHTML = "shopping_bag"
-        }, 2000)
-        icon.style.transform = "translateX(150px)"
-    }, 2000);
-}
 
 
 
